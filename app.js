51b062bc1db247467dcd7c23f95c8fffb4db409e
Aqsamk <aqsam.khan1999@gmail.com>
2023-01-27 03:16:10 +0530

    Booking app with own backend

--- app.js
+++ app.js
@@ -1,65 +1,62 @@
-const path = require('path');
-
 const express = require('express');
+const app = express();
 const bodyParser = require('body-parser');
-
-//const errorController = require('./controllers/error');
-
+const cors = require('cors');
 const sequelize = require('./util/database');
 const User = require('./models/User');
-const Form = require('./view/form');
 
-var Cors = require('cors');
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.urlencoded({extended:true}));
 
-const app = express();
+app.get('/api/get',(req,res,next) => {
 
-//app.set('view engine', 'ejs');
-//app.set('views', 'views');
-app.use(Cors());
+})
 
-//const adminRoutes = require('./routes/admin');
-//const shopRoutes = require('./routes/shop');
+app.get('/', (req,res,next) => {
+    res.send('Form is submitted')
+})
 
+app.get('/user/add-user', (req,res,next) => {
 
+    res.sendFile(__dirname+ "/view/form.html")
+    
+})
 
-app.use(bodyParser.json({ extended: false }));
-app.use(express.static(path.join(__dirname, 'Form')));
+app.post('/user/add-user', async (req,res,next) => {
 
-//app.use('/admin', adminRoutes);
-//app.use(shopRoutes);
-
-//app.use(errorController.get404);
-
-
-
-app.post('/user/add-user', async(req,res,next) => {
     try{
-
         if(!req.body.phonenumber){
             throw new Error('Phone number is mendatory')
         }
-    const name = req.body.name;
-    const email = req.body.email;
-    const phonenumber = req.body.phonenumber
 
-    const data = await User.create({name:name,email:email,phonenumber:phonenumber})
-    res.status(201).json({newUserDetail: data});
+   const name = req.body.name;
+   const email = req.body.email;
+   const phonenumber = req.body.phonenumber;
+
+   const data = await User.create({name:name,email:email,phonenumber:phonenumber})
+   res.status(201).json({newUserDetail:data});
+   //res.redirect('/user/get-users')
     }
     catch(err){
-        res.status(500).json({
-            err:err
-        })
+        console.log(err);
     }
+
+})
+
+app.get('/user/get-users', async (req,res,next) => {
+    const users = await User.findAll();
+    res.status(200).json({allUsers:users})
+})
+
+app.delete('/user/delete-user/:id', async (req,res,next) => {
+    const uId = req.params.id;
+    await User.destroy({where:{id:uId}})
+    res.sendStatus(200);
 })
 
 
 
-sequelize.sync()
-.then((res) => {
-   // console.log(res)
-    app.listen(5000); 
+app.listen(5000, () => {
+    console.log('server is running on port 5000')
 })
-.catch(err => console.log(err))
-
-
-
